54df9b89e9ed87fd8aa91e6f8dec7242704ebcc2
Prabhakar <prabhakarkr802@gmail.com>
2021-08-09 15:19:22 +0530

    All layouts done

---
+++ app/src/main/java/com/prabhakar/myapplication/Network.java
@@ -0,0 +1,4 @@
+package com.prabhakar.myapplication;
+
+public class Network {
+}
---
+++ app/src/main/java/com/prabhakar/myapplication/ResponseModel.java
@@ -0,0 +1,21 @@
+package com.prabhakar.myapplication;
+
+import java.io.Serializable;
+
+public class ResponseModel implements Serializable {
+	private String image;
+	private String subTitle;
+	private String title;
+
+	public String getImage(){
+		return image;
+	}
+
+	public String getSubTitle(){
+		return subTitle;
+	}
+
+	public String getTitle(){
+		return title;
+	}
+}
